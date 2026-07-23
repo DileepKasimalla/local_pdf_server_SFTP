66bae7a0d8e20787d1962cf3eb1bf6b4420e953d
DileepKasimalla <124340096+DileepKasimalla@users.noreply.github.com>
2026-07-23 11:37:25 +0530

    Add files via upload

---
+++ docs/js/config.js
@@ -0,0 +1,7 @@
+export const PORTAL_USER = "dileep";
+export const PORTAL_PASS = "Dileep123";
+export const TTL_DAYS = 5;
+
+export const GITHUB_REPO = "DileepKasimalla/local_pdf_server_SFTP";
+export const GITHUB_BRANCH = "main";
+export const PDFS_REPO_PATH = "docs/pdfs";
